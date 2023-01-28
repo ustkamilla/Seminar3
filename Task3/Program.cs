aa7f12993bba12b09ca5bc697f1895b606aa3399
Kamilla <ustkamilla@mail.ru>
2023-01-28 16:03:28 +0500

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 3
+
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+int InputInt(string message)
+{
+    Console.Write($"{message}: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int n = InputInt("Введите число");
+
+for (int i = 1; i <= n; i++)
+{
+    Console.Write($"{Math.Pow(i, 3)} ");
+}
